4667e5efc9a66ae19fa16096a9f422022eb02481
miguelferreirabranco <49939382+miguelferreirabranco@users.noreply.github.com>
2019-05-15 09:27:39 +0100

    Quarto Form

--- forms/Quartos.vb
+++ forms/Quartos.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Quartos
+    Dim i As Integer = 0
+    Private Sub Button_anterior_Click(sender As Object, e As EventArgs) Handles Button_anterior.Click
 
+    End Sub
+    Sub LoadFoarm()
+
+    End Sub
 End Class
