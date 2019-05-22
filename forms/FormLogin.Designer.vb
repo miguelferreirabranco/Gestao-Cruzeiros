4d3eb20fd6363941858632e78ae20f477a21e955
miguelferreirabranco <49939382+miguelferreirabranco@users.noreply.github.com>
2019-05-22 16:05:47 +0100

    alteracoes form login

--- forms/FormLogin.Designer.vb
+++ forms/FormLogin.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class FormLogin
     Inherits System.Windows.Forms.Form
 
     'Descartar substituições de formulário para limpar a lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class FormLogin
     'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
     'Pode ser modificado usando o Windows Form Designer.  
     'Não o modifique usando o editor de códigos.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
@@ -29,15 +29,17 @@ Partial Class FormLogin
         Me.Button_Adicionar = New System.Windows.Forms.Button()
         Me.Button_entrar = New System.Windows.Forms.Button()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.RadioButtonFuncionario = New System.Windows.Forms.RadioButton()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(165, 161)
+        Me.Label1.Location = New System.Drawing.Point(220, 198)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(82, 20)
+        Me.Label1.Size = New System.Drawing.Size(104, 25)
         Me.Label1.TabIndex = 51
         Me.Label1.Text = "Password:"
         '
@@ -45,32 +47,36 @@ Partial Class FormLogin
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(100, 129)
+        Me.Label2.Location = New System.Drawing.Point(133, 159)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(147, 20)
+        Me.Label2.Size = New System.Drawing.Size(182, 25)
         Me.Label2.TabIndex = 52
         Me.Label2.Text = "Nome de Utilizador:"
         '
         'TextBox_username
         '
-        Me.TextBox_username.Location = New System.Drawing.Point(269, 128)
+        Me.TextBox_username.Location = New System.Drawing.Point(359, 158)
+        Me.TextBox_username.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox_username.Name = "TextBox_username"
-        Me.TextBox_username.Size = New System.Drawing.Size(171, 20)
+        Me.TextBox_username.Size = New System.Drawing.Size(227, 22)
         Me.TextBox_username.TabIndex = 53
         '
         'TextBox_password
         '
-        Me.TextBox_password.Location = New System.Drawing.Point(269, 160)
+        Me.TextBox_password.Location = New System.Drawing.Point(359, 197)
+        Me.TextBox_password.Margin = New System.Windows.Forms.Padding(4)
         Me.TextBox_password.Name = "TextBox_password"
-        Me.TextBox_password.Size = New System.Drawing.Size(171, 20)
+        Me.TextBox_password.Size = New System.Drawing.Size(227, 22)
         Me.TextBox_password.TabIndex = 54
         '
         'Button_Adicionar
         '
         Me.Button_Adicionar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button_Adicionar.Location = New System.Drawing.Point(78, 288)
+        Me.Button_Adicionar.Location = New System.Drawing.Point(104, 354)
+        Me.Button_Adicionar.Margin = New System.Windows.Forms.Padding(4)
         Me.Button_Adicionar.Name = "Button_Adicionar"
-        Me.Button_Adicionar.Size = New System.Drawing.Size(88, 46)
+        Me.Button_Adicionar.Size = New System.Drawing.Size(117, 57)
         Me.Button_Adicionar.TabIndex = 55
         Me.Button_Adicionar.Text = "Novo Registo"
         Me.Button_Adicionar.UseVisualStyleBackColor = True
@@ -78,9 +84,10 @@ Partial Class FormLogin
         'Button_entrar
         '
         Me.Button_entrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button_entrar.Location = New System.Drawing.Point(335, 220)
+        Me.Button_entrar.Location = New System.Drawing.Point(447, 271)
+        Me.Button_entrar.Margin = New System.Windows.Forms.Padding(4)
         Me.Button_entrar.Name = "Button_entrar"
-        Me.Button_entrar.Size = New System.Drawing.Size(88, 39)
+        Me.Button_entrar.Size = New System.Drawing.Size(117, 48)
         Me.Button_entrar.TabIndex = 56
         Me.Button_entrar.Text = "Entrar"
         Me.Button_entrar.UseVisualStyleBackColor = True
@@ -88,17 +95,30 @@ Partial Class FormLogin
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(32, 258)
+        Me.Label3.Location = New System.Drawing.Point(43, 318)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(190, 13)
+        Me.Label3.Size = New System.Drawing.Size(252, 17)
         Me.Label3.TabIndex = 57
         Me.Label3.Text = "Ainda não tem conta? Registe-se aqui!"
         '
+        'RadioButtonFuncionario
+        '
+        Me.RadioButtonFuncionario.AutoSize = True
+        Me.RadioButtonFuncionario.Location = New System.Drawing.Point(104, 418)
+        Me.RadioButtonFuncionario.Name = "RadioButtonFuncionario"
+        Me.RadioButtonFuncionario.Size = New System.Drawing.Size(103, 21)
+        Me.RadioButtonFuncionario.TabIndex = 59
+        Me.RadioButtonFuncionario.TabStop = True
+        Me.RadioButtonFuncionario.Text = "Funcionario"
+        Me.RadioButtonFuncionario.UseVisualStyleBackColor = True
+        '
         'FormLogin
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(508, 376)
+        Me.ClientSize = New System.Drawing.Size(677, 463)
+        Me.Controls.Add(Me.RadioButtonFuncionario)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Button_entrar)
         Me.Controls.Add(Me.Button_Adicionar)
@@ -106,6 +126,7 @@ Partial Class FormLogin
         Me.Controls.Add(Me.TextBox_username)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "FormLogin"
         Me.Text = "Login"
         Me.ResumeLayout(False)
@@ -120,4 +141,5 @@ Partial Class FormLogin
     Friend WithEvents Button_Adicionar As Button
     Friend WithEvents Button_entrar As Button
     Friend WithEvents Label3 As Label
+    Friend WithEvents RadioButtonFuncionario As RadioButton
 End Class
--- forms/FormLogin.vb
+++ forms/FormLogin.vb
@@ -7,12 +7,87 @@
     End Sub
 
     Private Sub Button_Adicionar_Click(sender As Object, e As EventArgs) Handles Button_Adicionar.Click
-        Dim TempForm As New FormNovoRegisto_Viajante
+        Dim TempFormViajante As New FormNovoRegisto_Viajante
+        Dim TempFormFuncionario As New FormNovoRegisto_Funcionário
 
-        TempForm.Show()
+        If RadioButtonFuncionario.Checked Then
+            TempFormFuncionario.Show()
+        Else
+            TempFormViajante.Show()
+
+        End If
+
+
     End Sub
 
     Private Sub FormLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         VariavelGlobal.InitVars()
+    End Sub
+
+
+    Public Event LoginOK(ByVal NomeUser As String, ByVal Nivel As String)
+    Public Event LoginErro(ByVal NomeUser As String, ByVal Erro As String)
+
+    Private _loginok As Boolean = False
+
+    Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+        Dim loginok As Boolean
+        Dim Nivel As Integer
+
+        ValidaLogin(Me.TextBox_username.Text, Me.TextBox_password.Text, loginok, Nivel)
+        _loginok = loginok
+
+        If _loginok Then
+            RaiseEvent loginok(Me.TextBox_username.Text, Nivel)
+            Me.Close()
+        Else
+            MsgBox("Login Errado")
+
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+
+        _loginok = False
+        RaiseEvent LoginErro(Me.TextBox_username.Text, "Login Cancelado")
+        Me.Close()
+    End Sub
+
+
+
+    Private Sub LoginForm_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+
+    End Sub
+
+    Private Sub ValidaLogin(ByVal NomeUser As String, ByVal Password As String, ByRef loginok As Boolean, ByRef Nivel As Single)
+        loginok = False
+        For i = 0 To EmpresaCruzeiros.Cruzeiros(CruzeiroVisivel).Viajantes.Count - 1
+            If EmpresaCruzeiros.Cruzeiros(CruzeiroVisivel).Viajantes(i).Login = NomeUser Then
+                If EmpresaCruzeiros.Cruzeiros(CruzeiroVisivel).Viajantes(i).Password = Password Then
+                    ViajanteVisivel = i
+                    Nivel = "Viajante"
+                    MsgBox("Login efetuado com sucesso como viajante")
+                    loginok = True
+                    Exit Sub
+                End If
+            End If
+        Next
+
+        For i = 0 To EmpresaCruzeiros.Cruzeiros(CruzeiroVisivel).Funcionarios.Count - 1
+            If EmpresaCruzeiros.Cruzeiros(CruzeiroVisivel).Funcionarios(i).Login = NomeUser Then
+                If EmpresaCruzeiros.Cruzeiros(CruzeiroVisivel).Funcionarios(i).Password = Password Then
+                    FuncionarioVisivel = i
+                    Nivel = "Funcionario"
+                    MsgBox("Login efetuado com sucesso como funcionario")
+                    loginok = True
+                    Exit Sub
+                End If
+            End If
+        Next
+
     End Sub
 End Class
