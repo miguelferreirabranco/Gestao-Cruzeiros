c6dc0fba7458bfbce1296ebbf9d6d2b9ace7ebec
up201806706 <lidia.sb2000@gmail.com>
2019-05-25 14:58:06 +0100

    login

--- forms/FormLogin.Designer.vb
+++ forms/FormLogin.Designer.vb
@@ -37,7 +37,7 @@ Partial Class FormLogin
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-
+        Me.Label1.Location = New System.Drawing.Point(241, 204)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(82, 20)
         Me.Label1.TabIndex = 51
@@ -47,7 +47,7 @@ Partial Class FormLogin
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-
+        Me.Label2.Location = New System.Drawing.Point(241, 162)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(147, 20)
         Me.Label2.TabIndex = 52
@@ -55,14 +55,14 @@ Partial Class FormLogin
         '
         'TextBox_username
         '
-
+        Me.TextBox_username.Location = New System.Drawing.Point(394, 164)
         Me.TextBox_username.Name = "TextBox_username"
         Me.TextBox_username.Size = New System.Drawing.Size(171, 20)
         Me.TextBox_username.TabIndex = 53
         '
         'TextBox_password
         '
-
+        Me.TextBox_password.Location = New System.Drawing.Point(394, 206)
         Me.TextBox_password.Name = "TextBox_password"
         Me.TextBox_password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
         Me.TextBox_password.Size = New System.Drawing.Size(171, 20)
@@ -81,7 +81,7 @@ Partial Class FormLogin
         'Button_entrar
         '
         Me.Button_entrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-
+        Me.Button_entrar.Location = New System.Drawing.Point(477, 258)
         Me.Button_entrar.Name = "Button_entrar"
         Me.Button_entrar.Size = New System.Drawing.Size(88, 39)
         Me.Button_entrar.TabIndex = 56
@@ -101,7 +101,7 @@ Partial Class FormLogin
         '
         Me.RadioButtonFuncionario.AutoSize = True
         Me.RadioButtonFuncionario.Location = New System.Drawing.Point(45, 361)
-        Me.RadioButtonFuncionario.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.RadioButtonFuncionario.Margin = New System.Windows.Forms.Padding(2)
         Me.RadioButtonFuncionario.Name = "RadioButtonFuncionario"
         Me.RadioButtonFuncionario.Size = New System.Drawing.Size(80, 17)
         Me.RadioButtonFuncionario.TabIndex = 59
@@ -124,7 +124,7 @@ Partial Class FormLogin
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-
+        Me.ClientSize = New System.Drawing.Size(680, 423)
         Me.Controls.Add(Me.RadioButtonViajante)
         Me.Controls.Add(Me.RadioButtonFuncionario)
         Me.Controls.Add(Me.Label3)
