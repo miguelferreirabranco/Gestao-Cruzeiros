316b40c858c19ea3ad9c0e4a404b954a4d7e4480
Inês Queirós <iqp2000@gmail.com>
2019-05-25 15:44:22 +0100

    count

--- forms/FormListaQuartos.vb
+++ forms/FormListaQuartos.vb
@@ -2,10 +2,10 @@
 
     Private WithEvents FormQuartos As FormQuartos
     Private QuartoVisivel As Integer = 0
-    Private UltimaLinha As Integer = Me.DataGridView1.RowCount
+    Public UltimaLinha As Integer = Me.DataGridView1.Rows.Count()
 
     Private Sub FormListaQuartos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        DataGridView1.ColumnCount = 6
+        'DataGridView1.Rows.Count() = 6
         mostra()
     End Sub
 
@@ -22,8 +22,8 @@
         End While
 
         If temp = False Then
-            Me.DataGridView1.RowCount = Me.DataGridView1.RowCount + 1
-            UltimaLinha = Me.DataGridView1.RowCount
+            'Me.DataGridView1.RowCount = Me.DataGridView1.RowCount + 1
+            UltimaLinha = Me.DataGridView1.Rows.Count()
             Me.DataGridView1.Item(1, UltimaLinha).Value = nquarto
         End If
 
