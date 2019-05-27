222255f3790ddc1fe817ec3eca0190966765942a
up201806706 <lidia.sb2000@gmail.com>
2019-05-27 15:21:18 +0100

    FIMMMMMMM

--- forms/FormLogin.Designer.vb
+++ forms/FormLogin.Designer.vb
@@ -23,47 +23,29 @@ Partial Class FormLogin
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormLogin))
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox_username = New System.Windows.Forms.TextBox()
         Me.TextBox_password = New System.Windows.Forms.TextBox()
         Me.Button_Adicionar = New System.Windows.Forms.Button()
         Me.Button_entrar = New System.Windows.Forms.Button()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.RadioButtonFuncionario = New System.Windows.Forms.RadioButton()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.RadioButtonViajante = New System.Windows.Forms.RadioButton()
+        Me.RadioButtonFuncionario = New System.Windows.Forms.RadioButton()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(21, 62)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(82, 20)
-        Me.Label1.TabIndex = 51
-        Me.Label1.Text = "Password:"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(21, 20)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(147, 20)
-        Me.Label2.TabIndex = 52
-        Me.Label2.Text = "Nome de Utilizador:"
         '
         'TextBox_username
         '
-        Me.TextBox_username.Location = New System.Drawing.Point(174, 22)
+        Me.TextBox_username.Location = New System.Drawing.Point(573, 43)
         Me.TextBox_username.Name = "TextBox_username"
         Me.TextBox_username.Size = New System.Drawing.Size(171, 20)
         Me.TextBox_username.TabIndex = 53
         '
         'TextBox_password
         '
-        Me.TextBox_password.Location = New System.Drawing.Point(174, 64)
+        Me.TextBox_password.Location = New System.Drawing.Point(573, 85)
         Me.TextBox_password.Name = "TextBox_password"
         Me.TextBox_password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
         Me.TextBox_password.Size = New System.Drawing.Size(171, 20)
@@ -72,7 +54,7 @@ Partial Class FormLogin
         'Button_Adicionar
         '
         Me.Button_Adicionar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button_Adicionar.Location = New System.Drawing.Point(60, 154)
+        Me.Button_Adicionar.Location = New System.Drawing.Point(606, 349)
         Me.Button_Adicionar.Name = "Button_Adicionar"
         Me.Button_Adicionar.Size = New System.Drawing.Size(88, 46)
         Me.Button_Adicionar.TabIndex = 55
@@ -82,74 +64,113 @@ Partial Class FormLogin
         'Button_entrar
         '
         Me.Button_entrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button_entrar.Location = New System.Drawing.Point(257, 116)
+        Me.Button_entrar.Location = New System.Drawing.Point(656, 137)
         Me.Button_entrar.Name = "Button_entrar"
         Me.Button_entrar.Size = New System.Drawing.Size(88, 39)
         Me.Button_entrar.TabIndex = 56
         Me.Button_entrar.Text = "Entrar"
         Me.Button_entrar.UseVisualStyleBackColor = True
         '
-        'Label3
+        'PictureBox1
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 116)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(190, 13)
-        Me.Label3.TabIndex = 57
-        Me.Label3.Text = "Ainda não tem conta? Registe-se aqui!"
-        '
-        'RadioButtonFuncionario
-        '
-        Me.RadioButtonFuncionario.AutoSize = True
-        Me.RadioButtonFuncionario.Location = New System.Drawing.Point(25, 206)
-        Me.RadioButtonFuncionario.Margin = New System.Windows.Forms.Padding(2)
-        Me.RadioButtonFuncionario.Name = "RadioButtonFuncionario"
-        Me.RadioButtonFuncionario.Size = New System.Drawing.Size(80, 17)
-        Me.RadioButtonFuncionario.TabIndex = 59
-        Me.RadioButtonFuncionario.TabStop = True
-        Me.RadioButtonFuncionario.Text = "Funcionario"
-        Me.RadioButtonFuncionario.UseVisualStyleBackColor = True
+        Me.PictureBox1.Image = Global.forms.My.Resources.Resources.msc
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(767, 477)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 61
+        Me.PictureBox1.TabStop = False
         '
         'RadioButtonViajante
         '
         Me.RadioButtonViajante.AutoSize = True
+        Me.RadioButtonViajante.BackColor = System.Drawing.Color.LightSlateGray
         Me.RadioButtonViajante.BackgroundImage = CType(resources.GetObject("RadioButtonViajante.BackgroundImage"), System.Drawing.Image)
-        Me.RadioButtonViajante.Location = New System.Drawing.Point(110, 206)
+        Me.RadioButtonViajante.Location = New System.Drawing.Point(656, 405)
         Me.RadioButtonViajante.Name = "RadioButtonViajante"
         Me.RadioButtonViajante.Size = New System.Drawing.Size(63, 17)
-        Me.RadioButtonViajante.TabIndex = 60
+        Me.RadioButtonViajante.TabIndex = 66
         Me.RadioButtonViajante.TabStop = True
         Me.RadioButtonViajante.Text = "Viajante"
-        Me.RadioButtonViajante.UseVisualStyleBackColor = True
+        Me.RadioButtonViajante.UseVisualStyleBackColor = False
+        '
+        'RadioButtonFuncionario
+        '
+        Me.RadioButtonFuncionario.AutoSize = True
+        Me.RadioButtonFuncionario.BackColor = System.Drawing.Color.LightSlateGray
+        Me.RadioButtonFuncionario.Location = New System.Drawing.Point(571, 405)
+        Me.RadioButtonFuncionario.Margin = New System.Windows.Forms.Padding(2)
+        Me.RadioButtonFuncionario.Name = "RadioButtonFuncionario"
+        Me.RadioButtonFuncionario.Size = New System.Drawing.Size(80, 17)
+        Me.RadioButtonFuncionario.TabIndex = 65
+        Me.RadioButtonFuncionario.TabStop = True
+        Me.RadioButtonFuncionario.Text = "Funcionario"
+        Me.RadioButtonFuncionario.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Label3.Location = New System.Drawing.Point(554, 325)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(190, 13)
+        Me.Label3.TabIndex = 64
+        Me.Label3.Text = "Ainda não tem conta? Registe-se aqui!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(399, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(166, 20)
+        Me.Label2.TabIndex = 63
+        Me.Label2.Text = "Nome de Utilizador:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(464, 80)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 20)
+        Me.Label1.TabIndex = 62
+        Me.Label1.Text = "Password:"
         '
         'FormLogin
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(369, 234)
+        Me.ClientSize = New System.Drawing.Size(765, 476)
         Me.Controls.Add(Me.RadioButtonViajante)
         Me.Controls.Add(Me.RadioButtonFuncionario)
         Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button_entrar)
         Me.Controls.Add(Me.Button_Adicionar)
         Me.Controls.Add(Me.TextBox_password)
         Me.Controls.Add(Me.TextBox_username)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Name = "FormLogin"
         Me.Text = "Login"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
     Friend WithEvents TextBox_username As TextBox
     Friend WithEvents TextBox_password As TextBox
     Friend WithEvents Button_Adicionar As Button
     Friend WithEvents Button_entrar As Button
-    Friend WithEvents Label3 As Label
-    Friend WithEvents RadioButtonFuncionario As RadioButton
+    Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents RadioButtonViajante As RadioButton
+    Friend WithEvents RadioButtonFuncionario As RadioButton
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
 End Class
